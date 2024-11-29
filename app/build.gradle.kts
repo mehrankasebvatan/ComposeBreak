16dfbd05ce4f3fed8d124b4d3311f53a7d1ce80d
Mehran Kasebvatan <mehran.kasebvatan@gmail.com>
2024-11-29 22:05:27 +0330

    [refactor] sync gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
     implementation(project(":library:designsystem"))
+    implementation(project(":feature:countdown"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
